008a2f78dd0152e429aae608d31143695038cbcd
tmorgan <tmorgan@ba6c3d1a-52c9-43b0-8b7b-8793d2a55ffa>
2012-10-17 23:52:34 +0000

    made nextrand more user friendly
    git-svn-id: file:///bss/svn/public/bletchley@5 ba6c3d1a-52c9-43b0-8b7b-8793d2a55ffa

--- src/nextrand.c
+++ src/nextrand.c
@@ -4,6 +4,7 @@ Random.nextInt(), given any two sequential outputs of this method.
 
 Copyright (C) 2012 Virtual Security Research, LLC
 Author: Dan J. Rosenberg
+Updates by: Timothy D. Morgan
 
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License, version 3,
@@ -29,50 +30,50 @@ unsigned long long seed;
 
 int nextInt()
 {
-
-	seed = (seed * MULTIPLIER + ADDEND) & MASK;
-	return (int) (seed >> 16);
-
+  seed = (seed * MULTIPLIER + ADDEND) & MASK;
+  return (int) (seed >> 16);
 }
 
 
 int main(int argc, char **argv)
 {
-
-	int i, num;
-	unsigned long long r1, r2;
-
-
-	if (argc != 4) {
-		printf("[-] Usage: %s rand1 rand2 num\n", argv[0]);
-		return 1;
-	}
-
-	r1 = atoi(argv[1]);
-	r2 = atoi(argv[2]);
-	num = atoi(argv[3]);
-
-	for (i = 0; i < 65536; i++) {
-		seed = (r1 << 16) + i;
-		if ((unsigned int)(((seed * MULTIPLIER + ADDEND) & MASK) >> 16) == (unsigned int)r2) {
-			break;
-		}
-		seed = 0;
-	}
-
-	if (!seed) {
-		printf("[-] Seed not found.\n");
-		return 1;
-	}
-
-	/* Uncomment to print the first two values, which were already provided */
-//	printf("%d\n", (int)r1);
-//	printf("%d\n", nextInt());
-
-	for (i = 0; i < num; i++) {
-		printf("%d\n", nextInt());
-	}
-
-	return 0;
-
+  int i, num;
+  unsigned long long r1, r2;
+  
+  if (argc != 4) 
+  {
+    fprintf(stderr, "[-] Usage: %s rand1 rand2 num\n", argv[0]);
+    fprintf(stderr, 
+            "[-] Note that rand1 and rand2 must be signed integers returned in sequence\n"
+            "    from a single Java Random instance.  Values provided must be generated\n"
+            "    by Random.nextInt() which was called with no arguments.\n");
+    return 1;
+  }
+  
+  r1 = atoi(argv[1]);
+  r2 = atoi(argv[2]);
+  num = atoi(argv[3]);
+  
+  for (i = 0; i < 65536; i++) 
+  {
+    seed = (r1 << 16) + i;
+    if ((unsigned int)(((seed * MULTIPLIER + ADDEND) & MASK) >> 16) == (unsigned int)r2) 
+      break;
+    
+    seed = 0;
+  }
+  
+  if (!seed) {
+    fprintf(stderr, "[-] Seed not found.\n");
+    return 1;
+  }
+  
+  fprintf(stderr, "[+] Seed %.12llX found based on provided values: ", seed);
+  fprintf(stderr, "%d %d\n", (int)r1, nextInt());
+  fprintf(stderr, "[+] Next %d values:\n", num);
+  
+  for (i = 0; i < num; i++) 
+    printf("%d\n", nextInt());
+  
+  return 0;
 }
